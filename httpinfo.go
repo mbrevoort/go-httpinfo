48bba9419880f7fe3a28fd08b9749805a109cf6b
Mike Brevoort <mike.brevoort@robotsandpencils.com>
2015-07-12 23:58:34 -0600

    comments for godoc

--- httpinfo.go
+++ httpinfo.go
@@ -31,17 +31,17 @@ type HTTPInfo interface {
 //
 // For example:
 //
-//     func FooMiddleware(h http.Handler) http.Handler {
-//     	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//    func FooMiddleware(h http.Handler) http.Handler {
+//      return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 //
-//     		// wrap the request and track the response status code, size and duration
-//     		info := httpinfo.New(h)
-//     		info.ServeHTTP(w, r)
+//        // wrap the request and track the response status code, size and duration
+//        info := httpinfo.New(h)
+//        info.ServeHTTP(w, r)
 //
-//     		// log response
-//         fmt.Printf("Request: %s %s %s %d %d (%d) ", r.Method, r.RequestURI, r.Proto, info.Status(), info.Size(), info.Elapsed())
-//     	})
-//     }
+//        // log response
+//        fmt.Printf("Request: %s %s %s %d %d (%d) ", r.Method, r.RequestURI, r.Proto, info.Status(), info.Size(), info.Elapsed())
+//      })
+//    }
 func New(h http.Handler) HTTPInfo {
 	l := httpInfo{h: h}
 	return &l
