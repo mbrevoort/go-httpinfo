109b4484c005b5ec7dd2e13e6b284902542ab02e
Mike Brevoort <mike.brevoort@robotsandpencils.com>
2015-07-12 23:55:25 -0600

    comments for godoc

--- httpinfo.go
+++ httpinfo.go
@@ -5,6 +5,23 @@ import (
 	"time"
 )
 
+// Simple http.Handler middleware that records HTTP status code, response size, and duration and makes
+// the data available after `ServeHTTP` is finished. Requires no 3rd party dependencies.
+//
+// For example:
+//
+//     func FooMiddleware(h http.Handler) http.Handler {
+//     	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//
+//     		// wrap the request and track the response status code, size and duration
+//     		info := httpinfo.New(h)
+//     		info.ServeHTTP(w, r)
+//
+//     		// log response
+//         fmt.Printf("Request: %s %s %s %d %d (%d) ", r.Method, r.RequestURI, r.Proto, info.Status(), info.Size(), info.Elapsed())
+//     	})
+//     }
+
 // HTTPInfo is wrapper of http.ResponseWriter that keeps track of its HTTP status
 // code and body size
 type httpInfo struct {
